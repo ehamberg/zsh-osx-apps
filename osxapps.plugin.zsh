3e25a8af5405fc94d7225059f95af33d5adda028
Erlend Hamberg <erlend@hamberg.no>
2016-01-10 18:24:15 +0100

    Don't fail on empty `~/Applications`
    Set the NULL_GLOB option for the *.app pattern to avoid failing when
    `$HOME/Application/*.app` doesn't return any matches.

--- osxapps.plugin.zsh
+++ osxapps.plugin.zsh
@@ -1,7 +1,7 @@
 # Find all apps in /Applicatons and $HOME/Applications and create a shell
 # function with that name (in lowercase) that launches the app with the given
 # arguments
-for a in {$HOME,}/Applications/*.app ; do
+for a in {$HOME,}/Applications/*.app(N) ; do
 	eval "\${\${a:t:l:r}//[ -]/}() {\
 		if (( \$# == 0 )); then\
 			open ${(qq)a};\
