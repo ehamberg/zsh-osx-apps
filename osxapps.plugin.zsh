286ed7a9d63d2bdc1f4d892825e159c2182aed89
Erlend Hamberg <erlend@hamberg.no>
2017-03-08 20:34:03 +0100

    Append “app” to command name

--- osxapps.plugin.zsh
+++ osxapps.plugin.zsh
@@ -1,8 +1,8 @@
 # Find all apps in /Applicatons and $HOME/Applications and create a shell
-# function with that name (in lowercase) that launches the app with the given
-# arguments
+# function with that name (in lowercase), followed by "app" (e.g.
+# “pixelmatorapp”) that launches the app with the given arguments
 for a in {$HOME,}/Applications/*.app(N) ; do
-	eval "\${\${a:t:l:r}//[ -]/}() {\
+	eval "\${\${a:t:l:r}//[ -]/}app() {\
 		if (( \$# == 0 )); then\
 			open ${(qq)a};\
 		else\
@@ -10,4 +10,3 @@ for a in {$HOME,}/Applications/*.app(N) ; do
 		fi\
 	}"
 done
-
